815fe19e2ec0ca06a21bedf116d1364785993a68
Joel Lisenby <joel.lisenby@gmail.com>
2017-08-24 15:47:14 -0700

    change url filtering

--- scan.php
+++ scan.php
@@ -9,18 +9,20 @@ try {
 
 header('Content-Type: application/json');
 
-if(filter_var($_GET['url'], FILTER_VALIDATE_URL)) {
+$url = filter_var($_GET['url'], FILTER_VALIDATE_URL);
+
+if($url) {
 	$stmt = $db->prepare('SELECT * FROM webscanner.scans WHERE url = :url ORDER BY date DESC');
-	$stmt->execute(array('url'=>$_GET['url']));
+	$stmt->execute(array('url'=>$url));
 	$scan = $stmt->fetchAll(PDO::FETCH_ASSOC);
 	if(!empty($scan[0]) && strtotime($scan[0]['date']) > strtotime('-15 days')) {
 		echo $scan[0]['result'];
 	} else {
-		$cmd = 'lighthouse '. $_GET['url'] .' --output json --chrome-flags="--headless"';
+		$cmd = 'lighthouse '. $url .' --output json --chrome-flags="--headless"';
 		$output = shell_exec($cmd);
 		if(!empty($output)) {
 			$stmt = $db->prepare('INSERT INTO webscanner.scans (`date`, url, result) VALUES (:now, :url, :result);');
-			$stmt->execute(array('now'=>date("Y-m-d H:i:s"),'url'=>$_GET['url'],'result'=>$output));
+			$stmt->execute(array('now'=>date("Y-m-d H:i:s"),'url'=>$url,'result'=>$output));
 			echo $output;
 		} else {
 			echo json_encode(array("error"=>true,"message"=>"scan fail"));
